3dfcebad99cd9fa6bf78ce05923d31cbd1c81b37
Maira Wenzel <mairaw@microsoft.com>
2021-11-08 18:50:35 -0800

    add implicit usings

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 ﻿using dotnetthanks;
 using Microsoft.Extensions.Configuration;
 using Octokit;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace dotnetthanks_loader
 {
