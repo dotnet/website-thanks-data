ffcb6d7fe761db8fd35b6092da2c27b4d62b86b3
danzhu54 <dan.zhu@neudesic.com>
2023-01-10 15:20:46 -0800

    Consolidate contribs into major versions

--- Release.cs
+++ Release.cs
@@ -17,7 +17,8 @@ namespace dotnetthanks
             "v3.0.0",
             "v3.1.0",
             "v5.0.0",
-            "v6.0.0"
+            "v6.0.0",
+            "v7.0.0"
         };
         private string _tag;
 
@@ -46,6 +47,7 @@ namespace dotnetthanks
 
         public Version Version { get; private set; }
         public string VersionLabel { get; private set; }
+        public List<string> ProcessedReleases { get; set; } = new List<string>();
         public bool Equals(Release other)
         {
             if (other is null)
